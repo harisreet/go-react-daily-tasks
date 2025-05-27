5630efc640141c0dbe6962588eb9b808c3695270
Harisree T <harisree352006@gmail.com>
2025-05-27 10:43:43 +0530

    3rd section completed - ready for deployment

---
+++ client/src/main.tsx
@@ -0,0 +1,17 @@
+import * as React from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App.tsx';
+import { ChakraProvider } from '@chakra-ui/react';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import theme from "./chakra/theme.ts";
+const queryClient = new QueryClient() ;
+createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+     <QueryClientProvider client = {queryClient} >
+
+    <ChakraProvider theme={theme}>
+      <App />
+    </ChakraProvider>
+     </QueryClientProvider>
+  </React.StrictMode>
+);
--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 	"os"
 
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
 	"github.com/joho/godotenv"
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
@@ -17,19 +18,21 @@ import (
 type Todo struct {
 	ID        primitive.ObjectID `json:"_id,omitempty" bson:"_id,omitempty"`
 	Completed bool               `json:"completed"`
-	Body      string             `json:"body" `
+	Body      string             `json:"body"`
 }
 
 var collection *mongo.Collection
 
 func main() {
-	fmt.Println("hello world")
-	err := godotenv.Load(".env")
+	fmt.Println("hello!!")
 
-	if err != nil {
-		log.Fatal("Error loading .env file:", err)
+	if os.Getenv("ENV") != "production" {
+		//Load the .env file if not production
+		err := godotenv.Load(".env")
+		if err != nil {
+			log.Fatal("Error loading the .env file!", err)
+		}
 	}
-
 	MONGODB_URI := os.Getenv("MONGODB_URI")
 	clientOptions := options.Client().ApplyURI(MONGODB_URI)
 	client, err := mongo.Connect(context.Background(), clientOptions)
@@ -47,9 +50,14 @@ func main() {
 
 	fmt.Println("Connected to MONGODB ATLAS")
 
-	collection = client.Database("golang_db").Collection("todos")
+	collection = client.Database("golang_todo_db").Collection("todos")
 
 	app := fiber.New()
+
+	app.Use(cors.New(cors.Config{
+		AllowOrigins: "http://localhost:5173",
+		AllowHeaders: "Origin, Content-Type, Accept",
+	}))
 
 	app.Get("/api/todos", getTodos)
 	app.Post("/api/todos", createTodo)
@@ -61,6 +69,9 @@ func main() {
 		port = "5000"
 	}
 
+	if os.Getenv("ENV") == "production" {
+		app.Static("/", "./client/dist")
+	}
 	log.Fatal(app.Listen("0.0.0.0:" + port))
 
 }
@@ -81,31 +92,27 @@ func getTodos(c *fiber.Ctx) error {
 		if err := cursor.Decode(&todo); err != nil {
 			return err
 		}
+
 		todos = append(todos, todo)
 	}
-
 	return c.JSON(todos)
 }
-
 func createTodo(c *fiber.Ctx) error {
 	todo := new(Todo)
-	// {id:0,completed:false,body:""}
 
 	if err := c.BodyParser(todo); err != nil {
 		return err
 	}
-
 	if todo.Body == "" {
-		return c.Status(400).JSON(fiber.Map{"error": "Todo body cannot be empty"})
+		return c.Status(400).JSON(fiber.Map{"error": "Todo Body can't be empty"})
 	}
 
 	insertResult, err := collection.InsertOne(context.Background(), todo)
+
 	if err != nil {
 		return err
 	}
-
 	todo.ID = insertResult.InsertedID.(primitive.ObjectID)
-
 	return c.Status(201).JSON(todo)
 }
 func updateTodo(c *fiber.Ctx) error {
@@ -118,22 +125,19 @@ func updateTodo(c *fiber.Ctx) error {
 
 	filter := bson.M{"_id": objectID}
 	update := bson.M{"$set": bson.M{"completed": true}}
-
 	_, err = collection.UpdateOne(context.Background(), filter, update)
+
 	if err != nil {
 		return err
 	}
-
 	return c.Status(200).JSON(fiber.Map{"success": true})
-
 }
-
 func deleteTodo(c *fiber.Ctx) error {
 	id := c.Params("id")
 	objectID, err := primitive.ObjectIDFromHex(id)
 
 	if err != nil {
-		return c.Status(400).JSON(fiber.Map{"error": "Invalid todo ID"})
+		return c.Status(400).JSON(fiber.Map{"error": "Invalid Todo ID"})
 	}
 
 	filter := bson.M{"_id": objectID}
@@ -142,6 +146,5 @@ func deleteTodo(c *fiber.Ctx) error {
 	if err != nil {
 		return err
 	}
-
 	return c.Status(200).JSON(fiber.Map{"success": true})
 }
